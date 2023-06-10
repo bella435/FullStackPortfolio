b6bb718658ef30a524e99c272f2c29e408bc7f03
bella435 <mamadou.diallo19@lc.cuny.edu>
2023-06-10 17:20:32 -0400

    day5

---
+++ bmiproject/bmicalc.js
@@ -0,0 +1,5 @@
+function bmifunction(){
+    let w = document.getElementById('button2');
+         w = (703 * Weight) / Height;
+         return w;
+}
---
+++ hotelsproject/hotels.js
@@ -0,0 +1,12 @@
+function Hotel (name, rooms, booked, picture){
+    this.name = name;
+    this.rooms=rooms;
+    this.booked = booked;
+    this.picture = picture;
+    this.checkAvailability =
+               function ()
+               {   
+                   return this.rooms - this.booked;
+               };
+   
+}
